3fcadce28ba568a39b28411c1dbadd0c3f42374d
HandWend <yangicson@naver.com>
2022-05-12 15:38:38 +0900

    0512

--- src/test/java/com/kb/persistence/DataSourceTests.java
+++ src/test/java/com/kb/persistence/DataSourceTests.java
@@ -5,6 +5,8 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
+
 
 //version up
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -23,6 +26,17 @@ public class DataSourceTests {
 
 	@Setter(onMethod_ = {@Autowired})
 	private DataSource dataSource;
+	
+	@Setter(onMethod_ = {@Autowired})
+	private SqlSessionFactory sqlSessionFactory;
+	
+	//Test가 붙어있는 곳만 테스트한다.
+	@Test
+	public void testMyBatis() {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		Connection conn = sqlSession.getConnection();
+		log.info(conn);
+	}
 	
 	@Test
 	public void testConnection() {
